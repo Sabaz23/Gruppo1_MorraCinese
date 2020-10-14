a4be01b7b39d5d08bb83515fa6c270296f78edb6
Luca Sabatino <40960997+Sabaz23@users.noreply.github.com>
2020-10-14 16:40:21 +0200

    Update versione1.cpp

--- versione1.cpp
+++ versione1.cpp
@@ -10,61 +10,35 @@ using namespace std;
 int main(){
 
 	//Dichiaro le due giocate
-	char g1,g2;
+	int g1,g2;
 	//Dichiaro la variabile vincitore (0=Giocatore 1, 1=Giocatore 2, 2=Pareggio)
 	int vincitore;
 	
-	cout << "Per inserire una giocata,'c'=carta, 's'=sasso, 'f'= forbici" << endl;
+	cout << "Per inserire una giocata, 0=carta, 1=sasso, 2= forbici" << endl;
 	
 	//Continua a chiedere fino a quando l'input non è valido
 	do{
 		cout << "Inserisci la giocata del primo giocatore:" << endl;
 		cin >> g1;
-	}while(g1!='c' && g1!='s' && g1!= 'f');
+	}while(g1<0 || g1 >2);
 	
 	//Stesso per il secondo giocatore
 	do{
 		cout << "Inserisci la giocata del secondo giocatore:" << endl;
 		cin >> g2;
-	}while(g2!='c' && g2!='s' && g2!= 'f');
+	}while(g2<0 || g2>2);
 	
 	
 	if(g1==g2) //Se le giocate sono le stesse, pareggio
 	{
 		vincitore=2;
 	}
-	else if(g1 == 'f') //Giocatore 1 forbice
+	else if((g1+1)%3 == g2)
 	{
-		if(g2=='c')
-		{
-			vincitore=0; //Forbice vs Carta vince Forbice (G1)
-		}
-		else
-		{
-			vincitore=1; //Forbice vs Sasso vince Sasso (G2)
-		}
-	}
-	else if(g1 == 'c') //Giocatore 1 Carta
+		vincitore=0;
+	}else
 	{
-		if(g2=='s')
-		{
-			vincitore = 0; //Carta vs Sassso vince Carta (G1)
-		}
-		else
-		{
-			vincitore = 1; //Carta vs Forbice vince Forbice (G2)
-		}
-	}
-	else if(g1 == 's') //Giocatore 1 Sasso
-	{
-		if(g2=='f')
-		{
-			vincitore=0; //Sasso vs Forbice vince Sasso (G1)
-		}
-		else
-		{
-			vincitore=1; //Sasso vs Carta vince Carta (G2)
-		}
+		vincitore=1;
 	}
 	
 	switch(vincitore) //Verifico e stampo chi ha vinto tramite lo switch
@@ -82,4 +56,3 @@ int main(){
 	
 	
 }
-
